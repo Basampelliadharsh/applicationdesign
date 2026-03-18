89500c086431aab532142ae2a966da4e6a003f68
Basampelliadharsh <adharshbasampalli@gmail.com>
2026-03-18 18:25:12 +0530

    basics angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { Signup} from './signup/signup';
+import { Signin } from './signin/signin';
+import { Homepage } from './homepage/homepage';
+
+export const routes: Routes = [
+  { path: 'homepage',
+     component: Homepage },
+  { path: 'signup',
+     component: Signup },
+  { path: 'signin', 
+    component: Signin }
+];
