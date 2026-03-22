5de9b581a2aa3bff7211a29e005a1523052328c0
Basampelliadharsh <adharshbasampalli@gmail.com>
2026-03-22 19:18:33 +0530

    angular project practice

--- src/app/signin/signin.ts
+++ src/app/signin/signin.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -11,11 +12,20 @@ import { FormsModule } from '@angular/forms';
 export class Signin {
   email: string = '';
   password: string = '';
-  
+
+  constructor(private router: Router) {}
+
   onSubmit() {
     // Handle form submission logic here
     console.log('Email:', this.email);
     console.log('Password:', this.password);
+    this.router.navigate(['/signup']);
   }
-
+  togglePassword(input: HTMLInputElement) {
+    if (input.type === 'password') {
+      input.type = 'text';
+    } else {
+      input.type = 'password';
+    }
+  }
 }
--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './signup.html',
   styleUrls: ['./signup.css'],
 })
-export class Signup {}
+export class Signup {
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+constructor(private router: Router){}
+  onSubmit() {
+    console.log('Name:', this.name);
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    console.log('confirmPassword:', this.confirmPassword);
+    this.router.navigate(['/homepage']);
+  }
+
+  togglePassworddivisibility() {}
+}
