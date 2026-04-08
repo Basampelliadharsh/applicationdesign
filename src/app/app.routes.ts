7cde4211419a6ce444edbfa39c2a44e4561fba5a
Basampelliadharsh <adharshbasampalli@gmail.com>
2026-04-09 00:22:47 +0530

    student profile project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,18 @@
 import { Routes } from '@angular/router';
-import { Signup} from './signup/signup';
+import { Signup } from './signup/signup';
 import { Signin } from './signin/signin';
 import { Homepage } from './homepage/homepage';
 import { Libsignin } from './libsignin/libsignin';
 import { Libsignup } from './libsignup/libsignup';
 import { Libhomepage } from './libhomepage/libhomepage';
+import { ProfileFormPractice } from './profile-form-practice/profile-form-practice';
 
 export const routes: Routes = [
-  { path: 'homepage',
-     component: Homepage },
-  { path: 'signup',
-     component: Signup },
-  { path: 'signin', 
-    component: Signin },
-  { path: 'libhomepage',
-    component: Libhomepage },
-  { path: 'libsignup',
-    component: Libsignup },
-  { path: 'libsignin',
-    component: Libsignin }
+  { path: 'homepage', component: Homepage },
+  { path: 'signup', component: Signup },
+  { path: 'signin', component: Signin },
+  { path: 'libhomepage', component: Libhomepage },
+  { path: 'libsignup', component: Libsignup },
+  { path: 'libsignin', component: Libsignin },
+  { path: 'profile-form-practice', component: ProfileFormPractice },
 ];
