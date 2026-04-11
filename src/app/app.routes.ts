d4450717576d9cc39125ef7f9fd23ce48ca70873
Basampelliadharsh <adharshbasampalli@gmail.com>
2026-04-11 23:59:18 +0530

    project 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Libsignin } from './libsignin/libsignin';
 import { Libsignup } from './libsignup/libsignup';
 import { Libhomepage } from './libhomepage/libhomepage';
 import { ProfileFormPractice } from './profile-form-practice/profile-form-practice';
+import { EmployeeProfile } from './employee-profile/employee-profile';
 
 export const routes: Routes = [
   { path: 'homepage', component: Homepage },
@@ -15,4 +16,5 @@ export const routes: Routes = [
   { path: 'libsignup', component: Libsignup },
   { path: 'libsignin', component: Libsignin },
   { path: 'profile-form-practice', component: ProfileFormPractice },
+  {path: 'employee-profile', component: EmployeeProfile },  
 ];
