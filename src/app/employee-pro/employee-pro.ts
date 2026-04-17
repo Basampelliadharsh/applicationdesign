c190df8420759653bf66587aaca771708f6687f6
Basampelliadharsh <adharshbasampalli@gmail.com>
2026-04-17 22:36:02 +0530

    adding ts

--- src/app/employee-pro/employee-pro.ts
+++ src/app/employee-pro/employee-pro.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { first } from 'rxjs'; 
 
 @Component({
   selector: 'app-employee-pro',
-  imports: [],
+  imports: [CommonModule ],
   templateUrl: './employee-pro.html',
   styleUrl: './employee-pro.css',
 })
-export class EmployeePro {}
+export class EmployeePro {
+  employeeForm = new FormGroup({
+    fullName: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]),
+    dateofbirth: new FormControl('', [Validators.required]),  
+    gender: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{10}$')]),
+linkdin: new FormControl('', [Validators.required, Validators.pattern('^(https?://)?(www\\.)?linkedin\\.com/.*$')]),
+Experience: new FormControl('', [Validators.required, Validators.minLength(1)]),
+company: new FormControl('', [Validators.required]),
+CTC: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+Notice: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+street: new FormControl('', [Validators.required]),
+city: new FormControl('', [Validators.required]),
+state: new FormControl('', [Validators.required]),
+zipCode: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{5}$')]),
+skills: new FormArray([]),
+
+  })
+
+}
--- src/app/libhomepage/libhomepage.ts
+++ src/app/libhomepage/libhomepage.ts
@@ -4,6 +4,5 @@ import { Component } from '@angular/core';
   selector: 'app-libhomepage',
   imports: [],
   templateUrl: './libhomepage.html',
-  styleUrl: './libhomepage.css',
 })
 export class Libhomepage {}
